c1db74c2aa58547f2a2845ebbb8ffc4cf6d65c28
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-11-27 16:50:10 +0100

    UPDATE: new works img animation

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 import AnimatedTitle from "./AnimatedTitle";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 function Work(props) {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isInView, setIsInView] = useState(false);
+
+  const hiddenMask = `repeating-linear-gradient(to top, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 300px, rgba(0,0,0,1) 300px, rgba(0,0,0,1) 300px)`;
+  const visibleMask = `repeating-linear-gradient(to bottom, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 300px)`;
+
   return (
     <div className={`${props.bgColor} font-bebas`}>
       <div className="flex flex-col items-center justify-center min-h-screen py-20 text-white wrapper">
@@ -12,19 +20,31 @@ function Work(props) {
                 srcSet={`./assets/${props.img}.webp`}
                 className="object-cover rounded-3xl"
               /> */}
-          <img
-            src={`./assets/${props.img}.png`}
-            alt="works"
-            className="rounded-3xl work-img"
-          />
+
+          <motion.div
+            initial={false}
+            animate={
+              isLoaded && isInView
+                ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+            }
+            transition={{
+              duration: 1,
+            }}
+            viewport={{ once: true }}
+            onViewportEnter={() => setIsInView(true)}
+          >
+            <img
+              src={`./assets/${props.img}.png`}
+              alt={`${props.img} work`}
+              className="rounded-3xl work-img"
+              onLoad={() => setIsLoaded(true)}
+            />
+          </motion.div>
           {/* </picture> */}
           <div className="flex flex-col items-center justify-center uppercase">
-            {/* <h2 className="mt-8 uppercase work font-bebas letter-spacing-xs">
-              {`${props.name}`}
-            </h2> */}
-
             <h2 className="mt-8 overflow-hidden tracking-wide text-center uppercase work font-bebas w-min ">
-              <AnimatedTitle text={props.name} />
+              <AnimatedTitle text={`${props.name}`} />
             </h2>
             {props.tags.map((tag) => (
               <p key={tag} className="font-ppmori">
