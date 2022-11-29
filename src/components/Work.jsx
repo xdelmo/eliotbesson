71029e93edaad11958988edcd581396ec3974e84
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-11-29 12:51:30 +0100

    UPDATE: added desktop view

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -66,57 +66,69 @@ function Work(props) {
             </div>
           </div>
         </div>
+
         {/* DESKTOP VIEW */}
-        <div className="hidden w-full py-20 text-white wrapper sm:block">
-          <p className="flex items-center justify-center pb-8 overflow-hidden font-ppmori">
-            <AnimatedTextWord text={`${props.id}`} />
+        <div className="hidden sm:block">
+          <div className="flex flex-col justify-between w-full h-full min-h-screen py-20 text-white wrapper">
+            {/* counter */}
+            <p className="flex items-center justify-center overflow-hidden font-ppmori">
+              <AnimatedTextWord text={`${props.id}`} />
+              <span> ───── 05</span>
+            </p>
 
-            <span> ───── 05</span>
-          </p>
+            {/* blocco principale */}
+            {/* tags */}
 
-          {/* blocco principale */}
+            <div>
+              <div>
+                {props.tags.map((tag, index) => (
+                  <span
+                    key={tag}
+                    className="mb-2 text-xs font-thin leading-5 uppercase font-ppmori letter-spacing-xs"
+                  >
+                    {`${
+                      index !== props.tags.length - 1 ? `${tag} / ` : `${tag}`
+                    }`}
+                  </span>
+                ))}
+              </div>
 
-          {props.tags.map((tag, index) => (
-            <span
-              key={tag}
-              className="mb-2 text-xs font-thin leading-5 uppercase font-ppmori letter-spacing-xs"
-            >
-              {`${index !== props.tags.length - 1 ? `${tag} / ` : `${tag}`}`}
-            </span>
-          ))}
-          <div className="flex items-center justify-between">
-            <h2 className=" overflow-hidden tracking-wide uppercase letter-spacing-xs font-bebas text-[6rem] self-end">
-              {`${props.name}`}
-            </h2>
-            <motion.div
-              initial={false}
-              animate={
-                isLoaded && isInView
-                  ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
-                  : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
-              }
-              transition={{
-                duration: 1,
-              }}
-              viewport={{ once: true }}
-              onViewportEnter={() => setIsInView(true)}
-            >
-              <motion.div
-                whileHover={{
-                  scale: 1.1,
-                  boxShadow: "0px 20px 100px -10px rgba(66, 71, 91, 0.1)",
-                }}
-                whileTap={{ scale: 0.9 }}
-              >
-                <img
-                  src={`./assets/${props.img}.png`}
-                  alt={`${props.img} work`}
-                  className="cursor-pointer rounded-3xl work-img"
-                  onLoad={() => setIsLoaded(true)}
-                  onClick={() => window.open(props.link)}
-                />
-              </motion.div>
-            </motion.div>
+              <div className="flex items-center justify-between">
+                <h2 className=" overflow-hidden tracking-wide uppercase letter-spacing-xs font-bebas text-[6rem] self-end">
+                  {`${props.name}`}
+                </h2>
+                {/* image */}
+                <motion.div
+                  initial={false}
+                  animate={
+                    isLoaded && isInView
+                      ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+                      : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+                  }
+                  transition={{
+                    duration: 1,
+                  }}
+                  viewport={{ once: true }}
+                  onViewportEnter={() => setIsInView(true)}
+                >
+                  <motion.div
+                    whileHover={{
+                      scale: 1.1,
+                      boxShadow: "0px 20px 100px -10px rgba(66, 71, 91, 0.1)",
+                    }}
+                    whileTap={{ scale: 0.9 }}
+                  >
+                    <img
+                      src={`./assets/${props.img}.png`}
+                      alt={`${props.img} work`}
+                      className="cursor-pointer rounded-3xl work-img"
+                      onLoad={() => setIsLoaded(true)}
+                      onClick={() => window.open(props.link)}
+                    />
+                  </motion.div>
+                </motion.div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
