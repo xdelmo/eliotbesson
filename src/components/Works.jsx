355d3b6666a3c51d03c65d16807bb10f004c57e4
Emanuele <84925809+xdelmo@users.noreply.github.com>
2022-11-29 11:46:42 +0100

    UPDATE: works desktop view

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -32,18 +32,47 @@ function Works() {
       },
     },
   };
+  const placeholderText = [{ type: "heading2", text: "Selected" }];
 
+  const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025,
+      },
+    },
+  };
   return (
     <div id="works" className="bg-primary rounded-t-3xl">
       <div className="flex flex-col items-center justify-center min-h-screen text-white rounded-t-3xl wrapper">
-        <h2 className="overflow-hidden tracking-wide text-center uppercase work font-bebas">
-          <AnimatedTitle text="Selected Works" />
-        </h2>
-        <p className="flex items-center justify-center font-ppmori">
-          <AnimatedTextWord text="2020 ───── 2022" />
-          {/* 2020 <span className="line"></span>
-          2022 */}
-        </p>
+        {/* MOBILE VIEW */}
+        <div className="sm:hidden">
+          <h2 className="overflow-hidden tracking-wide text-center uppercase work font-bebas">
+            <AnimatedTitle text="Selected Works" />
+          </h2>
+          <p className="flex items-center justify-center font-ppmori">
+            <AnimatedTextWord text="2020 ───── 2022" />
+            {/* 2020 <span className="line"></span>
+            2022 */}
+          </p>
+        </div>
+        {/* DESKTOP VIEW */}
+        <div className="hidden w-full text-white sm:block">
+          <div className="flex flex-col justify-between w-full">
+            <h2 className="flex self-start uppercase letter-spacing-xs text-[11rem] font-bebas">
+              Selected
+            </h2>
+
+            <div className="flex items-end justify-between w-full ">
+              <div className="pb-24">
+                <AnimatedTextWord text="2020 ───── 2022" />
+              </div>
+              <h2 className="uppercase font-bebas letter-spacing-xs text-[11rem]">
+                {" "}
+                Works
+              </h2>
+            </div>
+          </div>
+        </div>
       </div>
 
       {worksInfo.map((workInfo) => (
